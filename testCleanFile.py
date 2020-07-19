5c04a408ea833b6e154b0866af150e00be3a3a0b
Bradly Goodner <46427195+bradlygoodner@users.noreply.github.com>
2020-07-19 15:40:56 -0500

    Testing Web Scraping

---
+++ testCleanFile.py
@@ -0,0 +1,9 @@
+university_towns = []
+with open('Datasets/university_towns.txt') as file:
+    for line in file:
+        if '[edit]' in line:
+            # Remember this `state` until the next is found
+            state = line
+        else:
+            # Otherwise, we have a city; keep `state` as last-seen
+            university_towns.append((state, line))
